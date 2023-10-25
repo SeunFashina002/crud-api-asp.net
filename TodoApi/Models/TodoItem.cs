94f63c8ab8a25dcf501f7f586babb1cb5520213c
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-25 13:27:36 -0700

    added TodoItem model

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
