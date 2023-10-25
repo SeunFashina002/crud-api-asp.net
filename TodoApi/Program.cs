2ef0827ebb794e011cbd99942061bc43d26db5de
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-25 13:25:31 -0700

    added db context service to DI container

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var _GetConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<TodoDbContext>(options => options.UseNpgsql(_GetConnectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
